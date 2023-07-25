e70426a159d09e602a923405451b2f6986240245
Harshdeep Mishra <harshdeepmishra82@gmail.com>
2023-07-26 00:44:17 +0530

    add smooth scroll

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
-// 1. SMOOTH SCROLL CODE
-var navMenu = document.querySelectorAll(".nav-menu a");
-var sections = document.querySelectorAll("main section");
-var interval;
-
-for(var i=0; i<navMenu.length; i++){
-    // console.log(navMenu);
-    navMenu[i].addEventListener("click", function(event){
-        event.preventDefault();
-        
-        var sectionName = this.textContent.trim().toLowerCase();
-        var element = document.getElementById(sectionName);
-
-        // APPROACH 1 : to pass arguments to function
-        // interval = setInterval(scrollVertically, 10, element);
-
-        // APPROACH 2 : 
-        interval = setInterval(function(){
-            scrollVertically(element);
-        }, 10);
-    });
-}
-
-function scrollVertically(element){
-    var yPosition = element.getBoundingClientRect()['y'];
-    if(yPosition <= 0){
-        clearInterval(interval);
-        return;
-    }
-
-    window.scrollBy(0, 50);
-}
-
-// 2. SMOOTH SKILL WIDTH FILLING CODE
+// 1. SMOOTH SKILL WIDTH FILLING CODE
 var skillsContainer = document.getElementById("skills-container");
 var skillsInnerBox = document.querySelectorAll("#skills .inner-box");
 var intervalId;
